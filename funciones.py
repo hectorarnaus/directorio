23fb7efdba9c4e33457281696a905c9331d4272f
Héctor Arnau <hector.arnaus@gmail.com>
2025-07-06 18:56:47 +0200

    Crear provincias

---
+++ funciones.py
@@ -0,0 +1,66 @@
+import random, re
+
+def slurguiza(texto):
+    texto=texto.strip()
+    texto=texto.lower()
+    texto=texto.replace("ñ","n")
+    texto=texto.replace("á","a")
+    texto=texto.replace("é","e")
+    texto=texto.replace("í","i")
+    texto=texto.replace("ó","o")
+    texto=texto.replace("ú","u")
+    texto=texto.replace("ü","u")
+    texto=texto.replace("de","")
+    texto=texto.replace("el","")
+    texto=texto.replace("la","")
+    texto=texto.replace("los","")
+    texto=texto.replace("las","")
+    texto=texto.replace(" ","-")
+    while texto.find("--")!=-1:
+        texto=texto.replace("--","-")
+    
+    return texto
+
+def spinner(s):
+     
+    while True:
+        s, n = re.subn('{([^{}]*)}',
+                    lambda m: random.choice(m.group(1).split("|")),
+                    s)
+        if n == 0: break
+    return s.strip()
+
+def crea_provincia(home,provincia,texto,imagen):
+   
+
+    res=('<!-- wp:html -->\n'
+        '\t<div class="migas">\n'
+        f'\t\t<p><a href="{home}">Inicio</a> &gt; {provincia}</p>\n'
+        '\t</div>\n'
+        '<!-- /wp:html -->\n'
+        f'<!-- wp:media-text {{"mediaPosition":"right","mediaId":{imagen.get_id()},"mediaLink":"https://calculadora-porcentajes-colombia.top/localidad/ciudad/#main","mediaType":"image"}} -->\n'
+        '<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile"><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Contenido…"} -->\n'
+        f'\t\t<p>{texto}</p>\n'
+        f'\t\t<!-- /wp:paragraph -->\n'
+        '\t</div>\n'
+        f'<figure class="wp-block-media-text__media"><img src="{imagen.get_url()}" alt="" class="wp-image-178 size-full"/></figure></div>\n'
+        '<!-- /wp:media-text -->\n'
+        '<!-- wp:group {"layout":{"type":"constrained"}} -->\n'
+        '\t<div class="wp-block-group">\n'
+        '\t\t<!-- wp:heading {"textAlign":"center"} -->\n'
+        f'\t\t\t<h2 class="wp-block-heading has-text-align-center">Todas las creperias de {provincia}</h2>\n'
+        '\t\t<!-- /wp:heading -->\n'
+        f'\t\t<!-- wp:dpt/display-post-types {{"taxonomy":"category","terms":["{provincia}"],"number":100,"styleSup":["title"],"showPgnation":true}} /-->\n'
+        '\t</div>\n'
+        '\t<!-- /wp:group -->\n')
+    return res
+
+def obten_nombre_provincia(imagen):
+   return imagen.split(".")[0].capitalize()
+
+def obten_texto_provincia(provincia):
+    with open('provincia.txt', 'r') as file :
+        texto_base = file.read()
+        texto=spinner(texto_base)
+        texto=texto.replace("*Provincia*",provincia)
+        return texto
--- main.py
+++ main.py
@@ -1,41 +1,32 @@
 import os
 import openpyxl 
-#from autowordpress import *
-
-def crea_provincia(home,provincia,texto,imagen):
-
-    res=('<!-- wp:html -->\n'
-        '\t<div class="migas">\n'
-        f'\t\t<p><a href="{home}">Inicio</a> &gt; {provincia}</p>\n'
-        '\t</div>\n'
-        '<!-- /wp:html -->\n'
-        '<!-- wp:media-text {"mediaPosition":"right","mediaId":178,"mediaLink":"https://calculadora-porcentajes-colombia.top/localidad/ciudad/#main","mediaType":"image"} -->\n'
-        '<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile"><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Contenido…"} -->\n'
-        f'\t\t<p>{texto}</p>\n'
-        f'\t\t<!-- /wp:paragraph -->\n'
-        '\t</div>\n'
-        f'<figure class="wp-block-media-text__media"><img src="{imagen}" alt="" class="wp-image-178 size-full"/></figure></div>\n'
-        '<!-- /wp:media-text -->\n'
-        '<!-- wp:group {"layout":{"type":"constrained"}} -->\n'
-        '\t<div class="wp-block-group">\n'
-        '\t\t<!-- wp:heading {"textAlign":"center"} -->\n'
-        f'\t\t\t<h2 class="wp-block-heading has-text-align-center">Todas las creperias de {provincia}</h2>\n'
-        '\t\t<!-- /wp:heading -->\n'
-        f'\t\t<!-- wp:dpt/display-post-types {{"taxonomy":"category","terms":["{provincia}"],"number":100,"styleSup":["title"],"showPgnation":true}} /-->\n'
-        '\t</div>\n'
-        '\t<!-- /wp:group -->\n')
-    return res
+from funciones import *
+from autowordpress import *
 
 
 
-#wc=WpConnection("https://calculadora-porcentajes-colombia.top//xmlrpc.php",'hector.arnaus@gmail.com','bolo3o,Eresgay')
-#wc.connect()
+
+
+wc=WpConnection("https://calculadora-porcentajes-colombia.top//xmlrpc.php",'hector.arnaus@gmail.com','bolo3o,Eresgay')
+wc.connect()
 
 etiquetas=['crepería','crepes']
-ciudades=[]
-provincias=[]
-#print(crea_provincia("http://www.creperias.com","Valencia","Este es un texto de prueba de la provincia",'https://calculadora-porcentajes-colombia.top/wp-content/uploads/2025/06/valencia.jpg'))
-print(obten_lista_municipios("castilla_leon_1.xlsx"))
+
+
+
+ruta=os.getcwd()+("/img")
+for img in os.listdir(ruta):
+    provincia=obten_nombre_provincia(img)
+    print(f"Creando el artículo de la provincia de {provincia}")
+    #texto_con_estado=cambia_estado(texto_base,estado)
+    #parrafos=texto_con_estado.split("\n")
+    wp_img=Image(ruta+"/"+img,f"Creperías en la provincia de {provincia}")
+    wp_img.upload(wc)
+    wp_article=WpPost(f"Creperías en la provincia de {provincia}")
+    #imagen=WpImage(wp_img)
+    wp_article.add_element(crea_provincia("https://calculadora-porcentajes-colombia.top",provincia,obten_texto_provincia(provincia),wp_img))
+    wc.publica_post(wp_article)
+
 
 '''creperias=openpyxl.load_workbook('creperias.xlsx')
 hoja_activa = creperias.active
